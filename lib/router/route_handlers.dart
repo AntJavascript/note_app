903ef49693a1f810b09f9ff7bec46520f2a6beff
XiaoShi <337265223@qq.com>
2023-07-26 15:03:41 +0800

    Update route_handlers.dart

--- lib/router/route_handlers.dart
+++ lib/router/route_handlers.dart
@@ -1,1 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:fluro/fluro.dart';
 
+var rootHandler = Handler(
+    handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
+  return HomeComponent();
+});

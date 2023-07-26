ab73f97112995734192dc25f11bf0878c0c2ab8a
XiaoShi <337265223@qq.com>
2023-07-26 15:44:49 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,18 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:note_app/provider/user_model.dart';
 
 // 自定义组件
 import 'Views/app.dart';
 
 void main() {
-  runApp(const App());
+  runApp(MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserInfo()),
+      ],
+      child: const App(),
+    ));
 
   if (Platform.isAndroid) {
     // 设置沉浸式状态栏

3ebe0c5850412989f9e6eedf1348a189e526e93b
XiaoShi <337265223@qq.com>
2023-07-26 15:45:19 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
 import 'package:note_app/provider/user_model.dart';
 
 // 自定义组件

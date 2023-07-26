e691a01f497a9eb7827849cc0644ed35ece7c860
XiaoShi <337265223@qq.com>
2023-07-26 15:12:09 +0800

    Update index.dart

--- lib/Views/Create/index.dart
+++ lib/Views/Create/index.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Create extends StatelessWidget {
+
+  const Create({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title:'记一笔',
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: const Text('记一笔'),
+    );
+  }
+}
 

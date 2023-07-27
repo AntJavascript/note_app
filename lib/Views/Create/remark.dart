72ac6f1a92900b731ccd337fd93e7fafa0f4d193
XiaoShi <337265223@qq.com>
2023-07-27 18:40:33 +0800

    Update remark.dart

--- lib/Views/Create/remark.dart
+++ lib/Views/Create/remark.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
 
+class RemarkInput extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => RemarkInputState();
+}
+
+class RemarkInputState extends State<RemarkInput>{
+
+  //文本编辑控制器
+  TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:TextField(
+        controller: _controller,
+        style: TailTypo().font_size(14.0).TextStyle()
+    ))
+  }
+}

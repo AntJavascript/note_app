457c4b4f910c2666c9d0e6ef6051fb156f7de2b5
XiaoShi <337265223@qq.com>
2023-07-27 15:53:34 +0800

    Update tag.dart

--- lib/Views/Create/tag.dart
+++ lib/Views/Create/tag.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:tailstyle/tailstyle.dart';
+
 class Tag extends StatefulWidget {
 
   final String text;
@@ -20,9 +24,20 @@ class TagState extends State<Tag>{
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child:Text(text)
-    )
-  }
 
+    // 文本颜色
+    Color text_color = isActive ? Color.fromARGB(255, 120, 120, 120) : Color.fromARGB(255, 220, 220, 220);
+    // 边框颜色
+    Color border_color = isActive ? Color.fromARGB(255, 120, 120, 120) : Color.fromARGB(255, 220, 220, 220);
+    // 背景颜色
+    Color bg_color = isActive ? Color.fromARGB(255, 120, 120, 120) : Color.fromARGB(255, 220, 220, 220);
+
+    return TailBox().border(border_color, 1).as((s) {
+      return s.bg(bg_color).rounded(30).as((s) {
+        return s.Container(
+          child:TailTypo().text_color(text_color).font_size(12.0).Text(text),
+        );
+      });
+    });
+  }
 }

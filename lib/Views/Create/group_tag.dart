181ad51931d77ff403f4a9f6e8b9ad5c134b5418
XiaoShi <337265223@qq.com>
2023-07-27 19:59:44 +0800

    Update group_tag.dart

--- lib/Views/Create/group_tag.dart
+++ lib/Views/Create/group_tag.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
 
+// tag 组件
+import 'tag.dart';
+
+class GroupTag extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => GroupTagState();
+}
+
+class GroupTagState extends State<GroupTag>{
+
+ const List<String> tags = ['吃饭', '网购', '超市', '交通', '房租', '电话', '医药', '人情', '其他'];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Wrap(children:tags..map((e) {
+        return Tag(e);
+      }).tolist())
+    )
+  }
+}

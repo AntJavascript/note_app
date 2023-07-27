ebcd2fd25cbf9847db3e680120235cae92ff841b
XiaoShi <337265223@qq.com>
2023-07-27 20:31:06 +0800

    Create title_cell.dart

---
+++ lib/Views/Create/title_cell.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:tailstyle/tailstyle.dart';
+
+class TitleCell extends StatelessWidget {
+
+  final String title;
+  const TitleCell({Key? key, this.title}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          TailBox().border(Colors.blue, 2).Container(),
+          TailTypo().text_color(Colors.black).Text(title)
+        ]
+      )
+    )
+  }
+}

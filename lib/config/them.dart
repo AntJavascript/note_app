feae6908bc8b0a8e15d27634b2e9619892716a47
XiaoShi <337265223@qq.com>
2023-08-29 14:46:37 +0800

    Update them.dart

--- lib/config/them.dart
+++ lib/config/them.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 const Map<String, dynamic> AppThem = {};
 
 AppThem["ThemColor"] = Colors.yellow;
+
 AppThem["ButtonBgColor"] = Colors.yellow;
 AppThem["ButtonDisableColor"] = Colors.gray;
+
 AppThem["LineSpaceColor"] = Colors.gray;
+
+AppThem["iconColor"] = Colors.white;
+AppThem["iconDisableColor"] = Colors.gray;
+AppThem["iconBgColor"] = Colors.yellow;
+AppThem["iconDisableBgColor"] = Colors.gray;
+
+AppThem["Income"] = Colors.red;
+AppThem["expend"] = Colors.green;

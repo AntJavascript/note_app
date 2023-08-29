b68950f04da94a66a488385036b068b1c6e482a3
XiaoShi <337265223@qq.com>
2023-08-29 14:57:05 +0800

    Update record_list.dart

--- lib/Views/Home/Widgets/Detail/record_list.dart
+++ lib/Views/Home/Widgets/Detail/record_list.dart
@@ -1,30 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:tailstyle/tailstyle.dart';
+
+// 全局配置
+import 'note_app/config/them.dart';
 
 class RecordList extends StatelessWidget {
   const RecordList({
     super.key,
-    this.title = '',
-    this.value = '',
-    this.color = const Color.fromARGB(255, 238, 16, 0),
+    this.list = [],
   });
 
-  final String title;
-  final String value;
-  final Color color;
+  final List<Map<String, dynamic>> list;
 
   @override
   Widget build(BuildContext context) {
-    const double fontSize = 12.0;
-    return RichText(
-      text: TextSpan(
-          text: title,
-          style: const TextStyle(color: Colors.grey, fontSize: fontSize),
-          children: [
-            TextSpan(
-              text: value,
-              style: TextStyle(color: color, fontSize: fontSize),
-            ),
-          ]),
-    );
+    return Column(children:list.map((item) {
+      return Item(item);
+    }).toList());
   }
+}
+
+Widget Item(item) {
+  return Row(
+    children:[
+      CusIcon(item.icon),
+      Column(
+        children:[
+          Title(item.tag),
+          Remark(item.remark),
+        ]
+      ),
+      Expanded(child: Amount(item.amount, item.type))
+    ]
+  )
+}
+
+Widget CusIcon(String value){
+  return TailBox().px(8).rounded(30).bg(AppThem.iconBgColor).Container(
+    child:Icon(
+      IconData(value, fontFamily: 'Schyler'),
+      size: 16, 
+      color: AppThem.iconColor
+  ));
+}
+
+Widget Title(String text) {
+  return TailTypo().font_size(16.0).text_color(Colors.black).Text(text);
+}
+
+Widget Remark(String text) {
+  return TailTypo().font_size(12.0).text_color(Colors.gray).Text(text);
+}
+
+Widget Amount(String text, String type) {
+  return TailTypo().font_size(16.0).text_color(type == 'income' ? AppThem["Income"] : AppThem["expend"]).Text(text);
 }

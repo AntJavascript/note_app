310f2dd5ec455492da474f87f2a817f308e3dbda
XiaoShi <337265223@qq.com>
2023-08-29 16:31:01 +0800

    Update budget_card.dart

--- lib/Views/Home/Widgets/budget_card.dart
+++ lib/Views/Home/Widgets/budget_card.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:tailstyle/tailstyle.dart';
+
+class BudgetCard extends StatefulWidget {
+  const BudgetCard({Key? key}) : super(key: key);
+  @override
+  State<BudgetCard> createState() => _BudgetCardState();
+}
+
+class _BudgetCardState extends State<BudgetCard> {
+  @override
+  Widget build(BuildContext context) {
+    return TailBox().px(16).rounded(30).bg(AppThem.iconBgColor).Container();
+  }
+}
 

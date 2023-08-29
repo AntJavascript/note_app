03e02dbdc748f582b5ec87a9684c722e7b17fc01
XiaoShi <337265223@qq.com>
2023-08-29 08:57:27 +0800

    Update group_tag.dart

--- lib/Views/Create/group_tag.dart
+++ lib/Views/Create/group_tag.dart
@@ -11,7 +11,7 @@ class GroupTag extends StatefulWidget {
 }
 
 class GroupTagState extends State<GroupTag> {
-  List<String> tags = ['日常餐饮', '网购', '日用品', '住房', '水电煤气', '通讯', '交通', '医疗', '数码产品', '娱乐', '人情', '教育', '转账', '支付宝', '微信', '信用卡', '其他'];
+  List<String> tags = ['日常餐饮', '网购', '日用品', '烟酒', '住房', '水电煤气', '通讯', '交通', '医疗', '数码产品', '娱乐', '人情', '教育', '转账', '支付宝', '微信', '信用卡', '其他'];
 
   @override
   void initState() {

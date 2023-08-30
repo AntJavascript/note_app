bd86bb85ea5cd345ea2d109294a000fd586c4dd2
XiaoShi <337265223@qq.com>
2023-08-30 10:11:10 +0800

    Update them.dart

--- lib/config/them.dart
+++ lib/config/them.dart
@@ -2,13 +2,21 @@ import 'package:flutter/material.dart';
 
 class AppConfig() {
   static Color themColor = Colors.yellow;
+  
+  // botton
   static Color buttonBgColor = Colors.yellow;
   static Color buttonDisableColor = Colors.grey;
+
+  // space
   static Color lineSpaceColor = Colors.grey;
+
+  // icon
   static Color iconColor = Colors.white;
   static Color iconDisableColor = Colors.grey;
   static Color iconBgColor = Colors.yellow;
   static Color iconDisableBgColor = Colors.grey;
+
+  // text
   static Color expend = Colors.red;
   static Color Income = Colors.green;
 }

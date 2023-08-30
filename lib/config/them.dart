3ea6ade14cf40580a618fcd9ba967663d74e699b
XiaoShi <337265223@qq.com>
2023-08-30 10:38:52 +0800

    Update them.dart

--- lib/config/them.dart
+++ lib/config/them.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
 
 class AppConfig() {
-  static Color themColor = Colors.yellow;
+  static Color themColor = Color.fromRGBO(247, 202, 0, 1);
   
   // botton
-  static Color buttonBgColor = Colors.yellow;
-  static Color buttonDisableColor = Colors.grey;
+  static Color buttonBgColor = Color.fromRGBO(247, 202, 0, 1);
+  static Color buttonDisableColor = Color.fromRGBO(236, 236, 236, 1);
+  static Color buttonTextColor = Color.fromRGBO(42, 42, 42, 1);
+  static Color buttonDisableTextColor = Color.fromRGBO(161, 161, 161, 1);
 
   // space
-  static Color lineSpaceColor = Colors.grey;
+  static Color lineSpaceColor = Color.fromRGBO(42, 42, 42, 1);
 
   // icon
-  static Color iconColor = Colors.white;
-  static Color iconDisableColor = Colors.grey;
-  static Color iconBgColor = Colors.yellow;
-  static Color iconDisableBgColor = Colors.grey;
+  static Color iconColor = Color.fromRGBO(255, 255, 255, 1);
+  static Color iconDisableColor = Color.fromRGBO(42, 42, 42, 1);
+  static Color iconBgColor = Color.fromRGBO(247, 202, 0, 1);
+  static Color iconDisableBgColor = Color.fromRGBO(42, 42, 42, 1);
 
   // text
-  static Color expend = Colors.red;
-  static Color Income = Colors.green;
+  static Color expend = Color.fromRGBO(255, 0, 0, 1);
+  static Color Income = Color.fromRGBO(0, 255, 0, 1);
 }

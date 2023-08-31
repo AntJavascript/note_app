fc301200d80145e886036519a6b1f53c8fb2203a
XiaoShi <337265223@qq.com>
2023-08-31 14:16:28 +0800

    Update them.dart

--- lib/config/them.dart
+++ lib/config/them.dart
@@ -5,35 +5,35 @@ class AppColorConfig {
   static MaterialColor themColor = Colors.yellow;
 
   // botton
-  static Color buttonBgColor = Color.fromRGBO(247, 202, 0, 1);
-  static Color buttonDisableColor = Color.fromRGBO(236, 236, 236, 1);
-  static Color buttonTextColor = Color.fromRGBO(42, 42, 42, 1);
-  static Color buttonDisableTextColor = Color.fromRGBO(161, 161, 161, 1);
+  static const Color buttonBgColor = Color.fromRGBO(247, 202, 0, 1);
+  static const Color buttonDisableColor = Color.fromRGBO(236, 236, 236, 1);
+  static const Color buttonTextColor = Color.fromRGBO(42, 42, 42, 1);
+  static const Color buttonDisableTextColor = Color.fromRGBO(161, 161, 161, 1);
 
   // space
-  static Color lineSpaceColor = Color.fromRGBO(42, 42, 42, 1);
+  static const Color lineSpaceColor = Color.fromRGBO(42, 42, 42, 1);
 
   // icon
-  static Color iconColor = Color.fromRGBO(255, 255, 255, 1);
-  static Color iconDisableColor = Color.fromRGBO(42, 42, 42, 1);
-  static Color iconBgColor = Color.fromRGBO(247, 202, 0, 1);
-  static Color iconDisableBgColor = Color.fromRGBO(42, 42, 42, 1);
+  static const Color iconColor = Color.fromRGBO(255, 255, 255, 1);
+  static const Color iconDisableColor = Color.fromRGBO(42, 42, 42, 1);
+  static const Color iconBgColor = Color.fromRGBO(247, 202, 0, 1);
+  static const Color iconDisableBgColor = Color.fromRGBO(42, 42, 42, 1);
 
   // text
-  static Color expendTextColor = Color.fromRGBO(53, 209, 53, 1);
-  static Color incomeTextColor = Color.fromRGBO(255, 0, 0, 1);
-  static Color titleColor = Color.fromRGBO(29, 29, 29, 1);
-  static Color contentColor = Color.fromRGBO(29, 29, 29, 1);
-  static Color labelColor = Color.fromRGBO(156, 156, 156, 1);
+  static const Color expendTextColor = Color.fromRGBO(53, 209, 53, 1);
+  static const Color incomeTextColor = Color.fromRGBO(255, 0, 0, 1);
+  static const Color titleColor = Color.fromRGBO(29, 29, 29, 1);
+  static const Color contentColor = Color.fromRGBO(29, 29, 29, 1);
+  static const Color labelColor = Color.fromRGBO(156, 156, 156, 1);
 }
 
 // 字体相关
 class AppFontSizeConfig {
-  static double amountInput = 42.0;
-  static double bigTitle = 24.0;
-  static double title = 16.0;
-  static double labelTitle = 14.0;
-  static double content = 14.0;
-  static double subTitle = 12.0;
-  static int fontPercent = 1;
+  static const double amountInput = 42.0;
+  static const double bigTitle = 24.0;
+  static const double title = 16.0;
+  static const double labelTitle = 14.0;
+  static const double content = 14.0;
+  static const double subTitle = 12.0;
+  static const int fontPercent = 1;
 }

d166cd2919418c43e0bfb5094fcb9ae7b99779fc
XiaoShi <337265223@qq.com>
2023-08-31 10:15:55 +0800

    Update title_cell.dart

--- lib/Views/Create/title_cell.dart
+++ lib/Views/Create/title_cell.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:tailstyle/tailstyle.dart';
 
+// 全局配置
+import 'package:note_app/config/them.dart';
+
 class TitleCell extends StatelessWidget {
   final String title;
   const TitleCell(this.title, {Key? key}) : super(key: key);
@@ -8,7 +11,7 @@ class TitleCell extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(children: [
-      TailBox().border(Colors.blue, 2).mx(6).Container(),
+      TailBox().border(AppColorConfig.themColor, 2).mx(6).Container(),
       TailTypo().text_color(Colors.black).Text(title)
     ]);
   }

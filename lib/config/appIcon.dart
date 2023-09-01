9d072419ce813f4487915feb9ff9d419ad0303dc
XiaoShi <337265223@qq.com>
2023-09-01 09:05:59 +0800

    Update appIcon.dart

--- lib/config/appIcon.dart
+++ lib/config/appIcon.dart
@@ -9,4 +9,23 @@ class AppIcons {
   static const car = AppIconData(0xe901);
   static const card = AppIconData(0xe902);
   static const communicate = AppIconData(0xe903);
+  static const iconDate = AppIconData(0xe904);
+  static const ecommerce = AppIconData(0xe905);
+  static const education = AppIconData(0xe906);
+  static const find = AppIconData(0xe907);
+  static const gift = AppIconData(0xe908);
+  static const holiday = AppIconData(0xe909);
+  static const housing = AppIconData(0xe90a);
+  static const hydropower = AppIconData(0xe90b);
+  static const medical = AppIconData(0xe90c);
+  static const necessities = AppIconData(0xe90d);
+  static const other = AppIconData(0xe90e);
+  static const redEnvelope = AppIconData(0xe90f);
+  static const restaurant = AppIconData(0xe910);
+  static const smoke = AppIconData(0xe911);
+  static const taix = AppIconData(0xe912);
+  static const transfer = AppIconData(0xe913);
+  static const travel = AppIconData(0xe91b);
+  static const warn = AppIconData(0xe91c);
+  static const weChart = AppIconData(0xe91d);
 }

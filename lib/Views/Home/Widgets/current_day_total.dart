7f6686450bac25a3ce8e0590bffe1698ff55b0da
XiaoShi <337265223@qq.com>
2023-09-01 16:01:12 +0800

    Update current_day_total.dart

--- lib/Views/Home/Widgets/current_day_total.dart
+++ lib/Views/Home/Widgets/current_day_total.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:tailstyle/tailstyle.dart';
+
+// 全局配置
+import 'package:note_app/config/them.dart';
+// 全局字体配置
+import 'package:note_app/config/appIcon.dart';
 
 // 自定义组件
 import 'package:note_app/Views/Component/title_cell.dart';
@@ -22,8 +28,23 @@ class _CurrentDayTotalState extends State<CurrentDayTotal> {
   @override
   Widget build(BuildContext context) {
     return Column(
-        children: [TitleCell("${widget.dateStr} 收支统计")]
+        children: [
+          TitleCell("${widget.dateStr} 收支统计"),
+          Row(children:[
+            Expanded(child: Income("今日支出", "0.00")),
+            Expanded(child: Income("今日收入", "0.00"))
+          ]),
+        ]
     );
   }
-  
+
+  Widget Income(String text, {String type = 'income'}) {
+    bool isIncome = type == 'income';
+    return TailTypo()
+        .font_size(16.0)
+        .text_color(isIncome
+            ? AppColorConfig.incomeTextColor
+            : AppColorConfig.expendTextColor)
+        .Text("${isIncome ? "-" : "+"}${text}");
+  }
 }

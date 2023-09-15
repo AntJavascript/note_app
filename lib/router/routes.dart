23efba6c53928d9c9939480818d3c111381a108d
XiaoShi <337265223@qq.com>
2023-09-15 11:40:05 +0800

    Update routes.dart

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -2,9 +2,14 @@ import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 import './route_handlers.dart';
 
+/**
+ * 路由配置
+*/
+
 class Routes {
   static String root = "/";
   static String create = "/create";
+  static String login = "/login";
 
   static void configureRoutes(FluroRouter router) {
     router.notFoundHandler = Handler(
@@ -14,5 +19,6 @@ class Routes {
     });
     router.define(root, handler: rootHandler);
     router.define(create, handler: createRouteHandler);
+    router.define(login, handler: loginRouteHandler);
   }
 }

33fd6283c934ebb36901f19e991f3d4d72a621e6
XiaoShi <337265223@qq.com>
2023-09-15 11:39:50 +0800

    Update route_handlers.dart

--- lib/router/route_handlers.dart
+++ lib/router/route_handlers.dart
@@ -2,12 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:fluro/fluro.dart';
 import 'package:note_app/Views/Home/home.dart';
 import 'package:note_app/Views/Create/index.dart';
+import 'package:note_app/Views/Login/login.dart';
+
+/**
+ * 路由处理函数
+*/
 
 var rootHandler = Handler(
     handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
   return Home();
 });
+
 var createRouteHandler = Handler(
     handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
   return Create();
+});
+
+var loginRouteHandler = Handler(
+    handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
+  return Login();
 });

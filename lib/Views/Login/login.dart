93d05bc4ac3fbe8e09b801e32e685a9992478ff5
XiaoShi <337265223@qq.com>
2023-09-21 10:21:40 +0800

    Update login.dart

--- lib/Views/Login/login.dart
+++ lib/Views/Login/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './formInput.dart';
+import './button.dart';
 
 class Login extends StatelessWidget {
   Login({super.key});
@@ -26,57 +28,11 @@ class Login extends StatelessWidget {
                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
                   FormInput(phoneController, false, "手机号码"),
                   FormInput(passwordController, true, "密码"),
-                  LoginBtn(() {
-                    login()
-                  })
+                  SubmitBtn(onClick: login)
             ]),
           )
         ],
       ),
     );
   }
-}
-
-// 输入框
-Widget FormInput(TextEditingController controller, bool isPwd, String hintText) {
-  return TextField(
-    controller: controller,
-    obscureText: isPwd
-    decoration: InputDecoration(
-      hintText: hintText,
-      border: UnderlineInputBorder(
-        borderSide: BorderSide(
-        width: 1, color: Colors.grey[300])),
-      focusedBorder: UnderlineInputBorder(
-        borderSide: BorderSide(
-          width: 1,
-          color: Theme.of(context)
-            .primaryColor))),
-  )
-}
-
-// 登录按钮
-Widget LoginBtn(Function onClick) {
-  return Material(
-    child: InkWell(
-      onTap: () {
-        onClick();
-      },
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius:
-              BorderRadius.all(Radius.circular(5.0)),
-          color: Theme.of(context).primaryColor,
-        ),
-        alignment: Alignment.center,
-        width: MediaQuery.of(context).size.width - 40,
-        height: 42.0,
-        child: Text(
-          '登录',
-          style: TextStyle(
-              fontSize: 18.0, color: Colors.white),
-        ),
-      ),
-    ),
-  )
 }

288ef65d39b8ab9aa51cf0c73f2c9a215cafe02e
XiaoShi <337265223@qq.com>
2023-09-21 16:10:32 +0800

    Update skin.dart

--- lib/Views/Skin/skin.dart
+++ lib/Views/Skin/skin.dart
@@ -1,1 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+// app主题
+import 'package:note_app/provider/skin_model.dart';
+import 'package:note_app/config/skinColors.dart';
+
+class SkinList extends StatefulWidget {
+  const SkinList({Key? key,}) : super(key: key);
+
+ @override
+  State<StatefulWidget> createState() => _SkinListState();
+
+  void change() {
+   
+  }
+}
+
+class _BottomBarState extends State<BottomBar> {
+  
+  @override
+  initState() {
+    super.initState();
+  }
+
+  void itemClick(Colors color) {
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const spacing = 10.0;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('换肤'),
+        elevation: 0,
+      ),
+      body: Consumer(builder: (BuildContext context, AppSkin appSkin, Widget child) {
+        return ListView(
+          children: listColor.map((item) => Container(
+            color: item
+          )).toList(),
+        )
+      }),
+    );
+  }
+}

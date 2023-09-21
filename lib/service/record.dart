c3f36f9c665d449cc5524bda6acf77e4c67c5f3a
XiaoShi <337265223@qq.com>
2023-09-21 17:26:06 +0800

    Create record.dart

---
+++ lib/service/record.dart
@@ -0,0 +1,32 @@
+import 'package:note_app/http.dart';
+import 'package:note_app/model/record.dart';
+
+class RecordService {
+  // 根据日期查询列表
+  getList() async {
+    // 开启日志打印
+    DioUtil.instance?.openLog();
+    
+    var res = await DioUtil().request('/record/list');
+    RecordEntity listData = RecordEntity.fromJson(res);
+    List<RecordEntity> list = [];
+
+    if (listData.data.length > 0) {
+      listData.data.map((item) {
+        list.add(item);
+      }).toList();
+    }
+    return list;
+  }
+
+  // 查询详情
+  getDetail(int id) async {
+    // 开启日志打印
+    DioUtil.instance?.openLog();
+    
+    var res = await DioUtil().request('/record/detail${id}');
+    RecordEntity data = RecordEntity.fromJson(res);
+    return data.data;
+  }
+
+}

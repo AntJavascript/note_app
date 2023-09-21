23ccc1aded348f1bfd64a8134f5e44d060c275d2
XiaoShi <337265223@qq.com>
2023-09-21 10:22:57 +0800

    Update register.dart

--- lib/Views/Login/register.dart
+++ lib/Views/Login/register.dart
@@ -1,1 +1,39 @@
+import 'package:flutter/material.dart';
+import './formInput.dart';
+import './button.dart';
 
+class Register extends StatelessWidget {
+  Register({super.key});
+
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  void register() {
+   
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const spacing = 10.0;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('注册'),
+        elevation: 0,
+      ),
+      body: ListView(
+        children: [
+          Container(
+            margin: EdgeInsets.all(16),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  FormInput(phoneController, false, "手机号码"),
+                  FormInput(passwordController, true, "邮箱"),
+                  FormInput(passwordController, true, "密码"),
+                  SubmitBtn(onClick: register)
+            ]),
+          )
+        ],
+      ),
+    );
+  }
+}

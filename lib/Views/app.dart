a9338ce0515b62577b5a9aa6be9db9acd4de1acb
XiaoShi <337265223@qq.com>
2023-09-21 16:10:06 +0800

    Update app.dart

--- lib/Views/app.dart
+++ lib/Views/app.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:fluro/fluro.dart';
+import 'package:provider/provider.dart';
 
 import 'package:note_app/router/application.dart';
 import 'package:note_app/router/routes.dart';
 
 // 全局配置
 import 'package:note_app/config/them.dart';
+// app主题颜色
+import 'package:note_app/provider/skin_model.dart';
 
 // 自定义组件
 import 'Home/home.dart';
@@ -22,7 +25,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
+        primarySwatch: Provider.of<AppSkin>(context).color,
       ),
       home: Home(),
       onGenerateRoute: Application.router.generator,

764a4eac160b6bf778847ea8dd84faf88f10aa7a
XiaoShi <337265223@qq.com>
2023-09-22 16:43:33 +0800

    Update budget.dart

--- lib/model/budget.dart
+++ lib/model/budget.dart
@@ -1,31 +1,32 @@
 class BudgetEntity {
-/*
-{
-  "id": 1,
-  "amount": 15.26,
-  "account": "15888888888"
-} 
-*/
 
   int id;
   double amount;
   String account;
+  String? accessToken;
+  String? refreshToken;
 
   BudgetEntity({
     this.id,
     this.amount,
     this.account,
+    this.accessToken,
+    this.refreshToken,
   });
   BudgetEntity.fromJson(Map<String, dynamic> json) {
     id = json['id']?.toInt();
     amount = json['amount']?.toDouble();
     account = json['account']?.toString();
+    accessToken = json['access_token']?.toString();
+    refreshToken = json['refresh_token']?.toString();
   }
   Map<String, dynamic> toJson() {
     final data = <String, dynamic>{};
     data['id'] = id;
     data['amount'] = amount;
     data['account'] = account;
+    data['access_token'] = accessToken;
+    data['refresh_token'] = refreshToken;
     return data;
   }
 }

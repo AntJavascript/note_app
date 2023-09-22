7bd61f2495dea34731bca2d4223d14599cff735c
XiaoShi <337265223@qq.com>
2023-09-22 10:56:07 +0800

    Update button.dart

--- lib/Views/Login/button.dart
+++ lib/Views/Login/button.dart
@@ -1,27 +1,19 @@
 /**
  * 登录、注册确定按钮
   参数：
-    @onClick 点击事件
     @text 按钮文字
 */
 
 import 'package:flutter/material.dart';
 
 class SubmitBtn extends StatelessWidget {
-  final TextEditingController? controller;
-  final Function? onClick;
   final String? text;
 
-  const SubmitBtn({this.onClick, this.text = '确定', super.key, this.controller});
+  const SubmitBtn({this.text = '确定', super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: InkWell(
-        onTap: () {
-          onClick!();
-        },
-        child: Container(
+    return Container(
           decoration: BoxDecoration(
             borderRadius: BorderRadius.all(Radius.circular(5.0)),
             color: Theme.of(context).primaryColor,
@@ -33,8 +25,6 @@ class SubmitBtn extends StatelessWidget {
             text!,
             style: TextStyle(fontSize: 18.0, color: Colors.white),
           ),
-        ),
-      ),
-    );
+        );
   }
 }

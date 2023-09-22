4b031a3b5373dfb715ec1ec8061db21b1004f75c
XiaoShi <337265223@qq.com>
2023-09-22 16:53:19 +0800

    Update login_service.dart

--- lib/service/login_service.dart
+++ lib/service/login_service.dart
@@ -1,14 +1,22 @@
 import 'package:note_app/http.dart';
 import 'package:note_app/model/common.dart';
+import 'package:note_app/model/login.dart';
 
 class LoginService {
   // 登录
-  login(Map<String dynamic> data) async {
+  static login(Map<String dynamic> data) async {
     // 开启日志打印
     DioUtil.instance?.openLog();
     
     var response = await DioUtil().request('/user/login', method: DioMethod.post, data: data);
-    CommonEntity result = CommonEntity.fromJson(response);
+
+
+    var obj = LoginEntity.fromJson(jsonDecode(response));
+    String encodedJson = jsonEncode(obj.toJson());
+    print(encodedJson);
+
+
+    LoginEntity result = LoginEntity.fromJson(response);
     return result;
   }
 

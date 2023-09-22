a26ee36a589f1236f8dfb81f71298f13ea42fc2b
XiaoShi <337265223@qq.com>
2023-09-22 10:56:52 +0800

    Update formInput.dart

--- lib/Views/Login/formInput.dart
+++ lib/Views/Login/formInput.dart
@@ -4,6 +4,7 @@
     @controller 控制器 （必传）
     @isPwd 是否密码输入框
     @hintText 默认提示语
+    @maxLength 最大输入长度
 */
 
 import 'package:flutter/material.dart';
@@ -12,14 +13,16 @@ class FormInput extends StatelessWidget {
   final TextEditingController? controller;
   final bool isPwd;
   final String? hintText;
+  final int? maxLength
 
   const FormInput(
-      {required this.controller, this.isPwd = false, this.hintText, super.key});
+      {required this.controller, this.maxLength, this.isPwd = false, this.hintText, super.key});
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: controller,
+      maxLength: maxLength
       obscureText: isPwd,
       decoration: InputDecoration(
           hintText: hintText,

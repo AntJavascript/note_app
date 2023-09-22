3d0c5bacd929d20bbdb42c3199ad6e52efeea20f
XiaoShi <337265223@qq.com>
2023-09-22 16:53:53 +0800

    Update login.dart

--- lib/Views/Login/login.dart
+++ lib/Views/Login/login.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
-import './formInput.dart';
-import './button.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:common_utils/common_utils.dart';
+
+// 路由配置
+import 'package:note_app/router/application.dart';
+
+// widget
+import './formInput.dart'; // 输入框
+import './button.dart'; // 按钮
+
+// service
+import 'package:note_app/service/login_service.dart';
 
 class Login extends StatelessWidget {
   Login({super.key});
@@ -8,7 +18,47 @@ class Login extends StatelessWidget {
   TextEditingController phoneController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
-  void login() {}
+  // 弹窗提示 
+  void _showSnackBar(BuildContext context, String content) {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(content),
+    ));
+  }
+
+  // 验证登录
+  void loginAction(BuildContext context) async {
+    
+    String phone = phoneController.text;
+    String password = passwordController.text;
+    SharedPreferences prefer = await SharedPreferences.getInstance();
+
+    // 验证手机号码
+    if (!RegexUtil.isMobileExact(phone)) {
+      _showSnackBar(context, "手机号码不合法");
+      return;
+    }
+    // 密码必须6-10位
+    if (password.length < 6 || password.length > 10) {
+      _showSnackBar(context, "密码必须6-10位");
+      return;
+    }
+    LoginService.login({
+      "phone": phone,
+      "password": password
+    }).then(data => {
+      print(data);
+      if (data.data == "200") {
+        // 保存token
+        prefer.setString("access_token", data.access_token);
+        prefer.setString("refresh_token", data.refresh_token);
+        prefer.setString("phone", phone);
+        Application.router.navigateTo(context, "/"); // 跳转首页
+      } else {
+        _showSnackBar(context, "登录失败");
+      }
+    })
+  } 
 
   @override
   Widget build(BuildContext context) {
@@ -24,10 +74,15 @@ class Login extends StatelessWidget {
             child:
                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
               FormInput(
-                  controller: phoneController, isPwd: false, hintText: "手机号码"),
+                  controller: phoneController, isPwd: false, hintText: "手机号码", maxLength: 11),
               FormInput(
                   controller: passwordController, isPwd: true, hintText: "密码"),
-              SubmitBtn(onClick: login)
+              InkWell(
+                onTap: () {
+                  loginAction(context);
+                },
+                child: SubmitBtn()
+              )
             ]),
           )
         ],

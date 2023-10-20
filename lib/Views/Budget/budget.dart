b0d901c8fdee6baf55534e9a7b72170d1d86b5ff
XiaoShi <337265223@qq.com>
2023-10-20 15:52:24 +0800

    Create budget.dart

---
+++ lib/Views/Budget/budget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class BudgetPage extends StatefulWidget {
+  const SkinPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _BudgetPageState();
+}
+
+class _BudgetPageState extends State<BudgetPage> {
+  @override
+  initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('预算'),
+        elevation: 0,
+      ),
+      body: Center(),
+    );
+  }
+}

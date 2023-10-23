d59833de061abb21652b368bbf0ea0e440947ad1
XiaoShi <337265223@qq.com>
2023-10-23 09:54:49 +0800

    Update home.dart

--- lib/Views/Home/home.dart
+++ lib/Views/Home/home.dart
@@ -34,7 +34,33 @@ class _HomePageState extends State<HomePage> {
         isLogin = true;
       });
     } else {
-      LoginDialog();
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+         showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('请先登录'),
+              content:Text('检测到你还未登陆，请先登录再进行操作。'),
+              actions:<Widget>[
+                FlatButton(
+                  child: Text('去登录'),
+                  onPressed: (){
+                    Application.router.navigateTo(context, "/login");
+                  },
+                ),
+                FlatButton(
+                  child: Text('退出应用'),
+                  onPressed: () async{
+                    await SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+                  },
+                ),
+              ],
+              elevation: 20,
+            );
+          },
+        );
+      }
     }
   }
 

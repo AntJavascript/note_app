56352b59244148da22c53232c766eb7ff8279708
XiaoShi <337265223@qq.com>
2023-10-24 16:12:20 +0800

    Update index.dart

--- lib/Views/Create/index.dart
+++ lib/Views/Create/index.dart
@@ -5,7 +5,6 @@ import 'package:note_app/router/application.dart';
 // 自定义组件
 import 'package:note_app/Views/Create/expend/expend_page.dart';
 import 'package:note_app/Views/Create/income/income_page.dart';
-import 'package:note_app/Views/Create/transfer/transfer_page.dart';
 
 class Create extends StatefulWidget {
   Create({super.key});
@@ -22,22 +21,35 @@ class _CreateState extends State<Create> {
 
   @override
   Widget build(BuildContext context) {
-    List tabs = ["支出", "收入", "转账"];
-    return Scaffold(
-      appBar: AppBar(
-          title: Text("记一笔"),
-          bottom: TabBar(
-            tabs: tabs.map((e) => Tab(text: e)).toList(),
+    List<Map<String, dynamic>> tabs = [
+      {
+        "text": "支出",
+        "type": "expend",
+      },
+      {
+        "text": "收入",
+        "type": "income",
+        "widget": Incomepage()
+      }
+    ];
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+            title: Text("记一笔"),
+            bottom: TabBar(
+              tabs: tabs.map((e) => Tab(text: e.text)).toList(),
+            ),
           ),
-        ),
-      body: TabBarView( //构建
-          children: tabs.map((e) {
-            return Container(
-                alignment: Alignment.center,
-                child: Text(e, textScaleFactor: 5),
-            );
-          }).toList(),
-        ),
+        body: TabBarView(
+            children: tabs.map((e) {
+              if (e.["type"] == "expend") {
+                return Expendpage();
+              }
+              return Incomepage();
+            }).toList(),
+          ),
+      )
     );
   }
 }

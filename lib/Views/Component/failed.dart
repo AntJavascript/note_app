24acf4747b3cbfd46b1e4f25c95e6973e1edef20
XiaoShi <337265223@qq.com>
2023-10-25 11:04:33 +0800

    Create failed.dart

---
+++ lib/Views/Component/failed.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:tailstyle/tailstyle.dart';
+
+// 全局配置
+import 'package:note_app/config/them.dart';
+
+class Failed extends StatelessWidget {
+  const Failed({Key? key, this.text = "加载失败，点击重试", this.height = 100, this.onClick}): super(key: key);;
+
+  final String? text;
+  final double? height;
+  final Function? onClick;
+
+  @override
+    Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        onClick!();
+      },
+      child: Container(
+        height: height,
+        alignment: Alignment.center,
+        child: TailTypo().font_size(14.0).text_color(AppColorConfig.labelColor).Text(text)
+      )
+    );
+  }
+}

f8570d616fe8205607c72625f9e24572be028770
XiaoShi <337265223@qq.com>
2023-10-25 17:21:23 +0800

    Update income_edit.dart

--- lib/Views/Detail/income_edit.dart
+++ lib/Views/Detail/income_edit.dart
@@ -6,6 +6,7 @@ import 'package:note_app/event/bus.dart';
 
 // 公共组件
 import 'package:note_app/Views/Component/title_cell.dart';
+import 'package:note_app/Views/Component/button.dart';
 
 // 自定义组件
 import 'package:note_app/Views/Create/amount_input.dart';
@@ -16,20 +17,49 @@ import 'package:note_app/Views/Create/date_picker_popup.dart';
 
 // service
 import 'package:note_app/service/record_service.dart';
+import 'package:note_app/model/record_model.dart';
 
 // 工具类
 import 'package:note_app/tools/show_snack.dart';
 
 class IncomeEdit extends StatefulWidget {
-  IncomeEdit({super.key});
+  IncomeEdit({Key? key, required this.id}): super(key: key);
+
+  final int id;
+
   @override
   State<IncomeEdit> createState() => _IncomeEditState();
 }
 
 class _IncomeEditState extends State<IncomeEdit> {
+
+  bool liading = true;
+  bool success = true;
+  var record_model detail
+
   @override
   initState() {
     super.initState();
+    getData();
+  }
+
+  getData() async {
+    setState(() => liading = true);
+    final data = await RecordService.getDetail(widget.id);
+    if(data.code == 200) {
+      setState(() {
+        detail = data;
+        liading = false;
+        success = true;
+      })
+    } else {
+      setState(() {
+        detail = null;
+        success = false;
+        liading = false;
+      })
+    }
+    return data;
   }
 
   GlobalKey dateKey = GlobalKey();
@@ -37,6 +67,7 @@ class _IncomeEditState extends State<IncomeEdit> {
   GlobalKey tagKey = GlobalKey();
   GlobalKey remarkKey = GlobalKey();
 
+  // 保存
   void submit(BuildContext context) {
     Map<String, dynamic> data = {};
 
@@ -53,22 +84,40 @@ class _IncomeEditState extends State<IncomeEdit> {
     data["remark"] = remark.controller.text;
     data["account"] = "15817351609";
 
-    RecordService.add(data).then((data) => {
-          if (data.code == 200)
-            {
-              Bus.eventBus.fire(const UpdateTotalEvent('record')),
-              Application.router.pop(context),
-              showSnackBar(context, '成功')
-            }
-          else
-            {showSnackBar(context, data.msg)}
-        });
+    RecordService.edit(data).then((data) => {
+      if (data.code == 200)
+        {
+          Bus.eventBus.fire(const UpdateTotalEvent('record')),
+          Application.router.pop(context),
+          showSnackBar(context, '成功')
+        }
+      else
+        {showSnackBar(context, data.msg)}
+    });
+  }
+
+  // 删除
+  void del(BuildContext context) {
+    RecordService.del(data).then((data) => {
+      if (data.code == 200)
+        {
+          Bus.eventBus.fire(const UpdateTotalEvent('record')),
+          Application.router.pop(context),
+          showSnackBar(context, '删除成功')
+        }
+      else
+        {showSnackBar(context, data.msg)}
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     const spacing = 10.0;
-    return ListView(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("编辑"),
+      ),
+      body: ListView(
         children: [
           Container(
             margin: EdgeInsets.all(16),
@@ -84,15 +133,25 @@ class _IncomeEditState extends State<IncomeEdit> {
               TitleCell(title: "备注"),
               SizedBox(height: spacing),
               RemarkInput(key: remarkKey), // 备注
-              SizedBox(height: spacing),
-              CreateButton(
-                  submit: () {
-                    submit(context);
-                  },
-                  text: "确定")
+              SizedBox(height: spacing)
             ]),
           )
         ],
-      );
+      ),
+      bottomNavigationBar: Container(
+          height: 44.0,
+          decoration: BoxDecoration(color: Colors.white),
+          child: Row(
+            children: <Widget>[
+              NoteButton(text: '删除', onClick: () {
+                del(context);
+              }),
+              NoteButton(text: '保存', onClick: () {
+                submit(context);
+              }),
+            ]
+          )
+      )
+    );
   }
 }

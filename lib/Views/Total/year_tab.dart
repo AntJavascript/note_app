e0b2a2a8b783d6eaf26a84176f5beb8161c82165
XiaoShi <337265223@qq.com>
2023-11-09 16:36:23 +0800

    Create year_tab.dart

---
+++ lib/Views/Total/year_tab.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TotalYaerTab extends StatefulWidget {
+  const TotalYaerTab({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _TotalYaerTabState();
+
+}
+
+class _TotalYaerTabState extends State<TotalYaerTab> {
+  @override
+  initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}

9d8408ea9268f6da88ee8a398968df188c92887b
XiaoShi <337265223@qq.com>
2023-11-09 16:14:13 +0800

    Create month_tab.dart

---
+++ lib/Views/Total/month_tab.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TotalMonthTab extends StatefulWidget {
+  const TotalMonthTab({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _TotalMonthTabState();
+
+}
+
+class _TotalMonthTabState extends State<TotalMonthTab> {
+  @override
+  initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}

0b61e3051a9cdd1733e45e9b061319d35bb18567
XiaoShi <337265223@qq.com>
2023-11-09 16:36:49 +0800

    Create total_tab.dart

---
+++ lib/Views/Total/total_tab.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TotalTab extends StatefulWidget {
+  const TotalTab({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _TotalTabState();
+
+}
+
+class _TotalTabState extends State<TotalTab> {
+  @override
+  initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}

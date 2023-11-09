eb1fa2750214d588e996b5205c3a29751fb5adc5
XiaoShi <337265223@qq.com>
2023-11-09 16:36:01 +0800

    Create day_tab.dart

---
+++ lib/Views/Total/day_tab.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TotalDayTab extends StatefulWidget {
+  const TotalDayTab({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _TotalDayTabState();
+
+}
+
+class _TotalDayTabState extends State<TotalDayTab> {
+  @override
+  initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}

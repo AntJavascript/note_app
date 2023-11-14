ef7f7f54488c57b511526d8a57184a23245cab86
XiaoShi <337265223@qq.com>
2023-11-14 15:01:57 +0800

    Create progress.dart

---
+++ lib/Views/Total/components/progress.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+// app主题颜色
+import 'package:note_app/provider/skin_model.dart';
+
+class Progress extends StatelessWidget {
+
+  final double value;
+
+  const Progress({Key? key, required this.value}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const double radius = 6;
+    return Container(
+      child: Stack(
+        children:[
+          Container(
+            height: 6.0,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.all(Radius.circular(radius))
+            )
+        ),
+        Position(child:
+          Container(
+              child: Text("${value.toString()}%"),
+              
+              decoration: BoxDecoration(
+                color: Provider.of<AppSkin>(context).color,
+                borderRadius: BorderRadius.all(Radius.circular(radius))
+              )
+            ))
+      ])
+    );
+  }
+}

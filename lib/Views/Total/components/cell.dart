7750241fd8b17bba45c0817f30f1969e5d661d4b
XiaoShi <337265223@qq.com>
2023-11-15 09:55:00 +0800

    Create cell.dart

---
+++ lib/Views/Total/components/cell.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Cell extends StatelessWidget {
+  final String title;
+  final String income;
+  final String expend;
+  const Item({Key? key, this.title, this.income, this.expend}) : super(key: key);
+}

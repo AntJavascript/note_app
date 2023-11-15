c56a596e6560265b6d6943f08824393c2371979a
XiaoShi <337265223@qq.com>
2023-11-15 10:14:50 +0800

    Update cell.dart

--- lib/Views/Total/components/cell.dart
+++ lib/Views/Total/components/cell.dart
@@ -1,8 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:tailstyle/tailstyle.dart';
+import 'package:provider/provider.dart';
+
+// 全局配置
+import 'package:note_app/config/them.dart';
 
 class Cell extends StatelessWidget {
+  
+  const Item({Key? key, this.title, this.income, this.expend}) : super(key: key);
+
   final String title;
   final String income;
   final String expend;
-  const Item({Key? key, this.title, this.income, this.expend}) : super(key: key);
+
+  @override
+    Widget build(BuildContext context) {
+      return TailBox().p(10).m(12).rounded(6).bg(Colors.white).Container(
+        child: Row(
+          children:[
+            TailTypo().font_size(16.0).Text(title),
+            Column(
+              children:[
+               Container(
+                 TailTypo().font_size(14.0).text_color(Colors.grey).Text("支出"),
+                 TailTypo().font_size(14.0).text_color(AppColorConfig.expendTextColor).Text(expend),
+               ),
+                Container(
+                 TailTypo().font_size(14.0).text_color(Colors.grey).Text("收入"),
+                 TailTypo().font_size(14.0).text_color(AppColorConfig.incomeTextColor).Text(income),
+               ),
+              ]
+            ),
+          ]
+        )
+      );
+    }
 }

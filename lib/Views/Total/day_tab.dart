7bba5849ecf55ab96bc6e3f6e974afa320b84b09
XiaoShi <337265223@qq.com>
2023-11-15 10:29:09 +0800

    Update day_tab.dart

--- lib/Views/Total/day_tab.dart
+++ lib/Views/Total/day_tab.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:note_app/Views/Home/Widgets/week.dart';
+import 'package:note_app/Views/Home/Widgets/item.dart';
+
 class TotalDayTab extends StatefulWidget {
   const TotalDayTab({
     Key? key,
@@ -10,13 +13,34 @@ class TotalDayTab extends StatefulWidget {
 }
 
 class _TotalDayTabState extends State<TotalDayTab> {
+
+  String dateStr = "";
+  
   @override
   initState() {
     super.initState();
   }
 
+  void onClick(String value) {
+    setState(() {
+      dateStr = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("data");
+    return return ListView(
+      children: [
+        Container(
+            color: Colors.white,
+            margin: const EdgeInsets.only(top: 10),
+            child: Column(
+              children: [
+                Week(),
+                Item(onClick: onClick),
+              ],
+            ))
+        ]
+        );
   }
 }

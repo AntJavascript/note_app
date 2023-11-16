553e5c9092b2bbabd9068e87a916d83fd452de71
XiaoShi <337265223@qq.com>
2023-11-16 14:24:19 +0800

    Update month_tab.dart

--- lib/Views/Total/month_tab.dart
+++ lib/Views/Total/month_tab.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+// 自定义组件
+import 'package:note_app/Views/Total/components/cell.dart';
+
 class TotalMonthTab extends StatefulWidget {
   const TotalMonthTab({
     Key? key,
@@ -10,6 +13,30 @@ class TotalMonthTab extends StatefulWidget {
 }
 
 class _TotalMonthTabState extends State<TotalMonthTab> {
+
+  List<Map<String, String>> list = [
+    {
+      "title": "1月",
+      "income": "0.0",
+      "expend": "0.0",
+    },
+    {
+      "title": "2月",
+      "income": "0.0",
+      "expend": "0.0",
+    },
+    {
+      "title": "3月",
+      "income": "0.0",
+      "expend": "0.0",
+    },
+    {
+      "title": "4月",
+      "income": "0.0",
+      "expend": "0.0",
+    }
+  ]
+     
   @override
   initState() {
     super.initState();
@@ -17,6 +44,10 @@ class _TotalMonthTabState extends State<TotalMonthTab> {
 
   @override
   Widget build(BuildContext context) {
-    return Text("data");
+    return ListView(
+      children: tabs.map((e) {
+        return Cell(title: e["title"], income: e["income"], expend: e["expend"]);
+      }).toList()
+    )
   }
 }
